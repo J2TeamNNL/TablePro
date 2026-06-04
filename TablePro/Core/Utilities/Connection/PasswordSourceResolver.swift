45075ac3f0af33dfa3a2fa3688f38361ba05e272
Ngo Quoc Dat <datlechin@gmail.com>
2026-06-05 01:24:03 +0700

    fix(connections): capture full output from a password command that exits quickly

--- TablePro/Core/Utilities/Connection/PasswordSourceResolver.swift
+++ TablePro/Core/Utilities/Connection/PasswordSourceResolver.swift
@@ -130,6 +130,13 @@ enum PasswordSourceResolver {
             stdoutPipe.fileHandleForReading.readabilityHandler = nil
             stderrPipe.fileHandleForReading.readabilityHandler = nil
 
+            if let remainingStdout = try? stdoutPipe.fileHandleForReading.readToEnd(), !remainingStdout.isEmpty {
+                stdoutCollector.append(remainingStdout)
+            }
+            if let remainingStderr = try? stderrPipe.fileHandleForReading.readToEnd(), !remainingStderr.isEmpty {
+                stderrCollector.append(remainingStderr)
+            }
+
             if stdoutCollector.overflowed {
                 throw ResolutionError.outputTooLarge
             }
